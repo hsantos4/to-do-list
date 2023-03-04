a5bc7a15ada86966f2ba9165845b8326736e3d8a
hsantos4 <hector.santos597@gmail.com>
2023-03-04 16:58:10 -0500

    Put all the code that is part of creating a task in a single function

--- index.js
+++ index.js
@@ -26,11 +26,9 @@ let tasks = document.getElementById('tasks');
 const removeTask = document.getElementById('remove-task')
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', createListItem)
-// btn.addEventListener('keydown', )
 
 // This function creates the list items and adds it to the body of the tasks
-function createListItem() {
+const createListItem = () => {
 
   let taskItem = document.createElement('li');
   // adding the class names to the list items
@@ -50,14 +48,10 @@ function createListItem() {
  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg></span>`;
+
 }
 
-// Creating a function if the user presses the 'Enter' key to submit the task
-function ifKeyDownEvent(event, createListItem) {
-
- }
-
-  
 
 
+btn.addEventListener('click', createListItem)
 
